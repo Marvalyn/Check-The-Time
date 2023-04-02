2921a3543acb3136e796b8926a96a830f0dff284
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-02 18:39:29 +0100

    added transforming properties to second hand

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function setDate() {
+
+    const secondHand = document.querySelector('.second-hand');
     //to get current number of seconds
     const now = new Date();
     const seconds = now.getSeconds();
     //to turn the number of seconds into base 100
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+    secondHand.style.transform =`rotate(${secondsDegrees}deg)`
+
+   
 }
 
 setInterval(setDate, 1000);
