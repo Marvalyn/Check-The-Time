cce52a4fac851704623211ebcdf0325b746657c0
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-02 19:36:06 +0100

    added comments to script file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function setDate() {
-
+    // selecting clock hands using the html classes
     const secondHand = document.querySelector('.second-hand');
     const minsHand = document.querySelector('.min-hand');
     const hourHand = document.querySelector('.hour-hand');
@@ -11,13 +11,20 @@ function setDate() {
     //add the styling to the second hand 
     secondHand.style.transform =`rotate(${secondsDegrees}deg)`
 
+    // to get current number of minutes
     const mins = now.getMinutes();
+    //to turn the number of minutes into base 100
     const minsDegrees = ((mins / 60) * 360) + 90;
+    //add the styling to the minute hand 
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+    // get current number of hours
     const hour = now.getHours();
+    //to turn the number of hours into base 100
     const hourDegrees = ((hour / 12) * 360) + 90;
+    //add the styling to the hour hand 
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
+// add interval so setDate function runs every second
 setInterval(setDate, 1000);
