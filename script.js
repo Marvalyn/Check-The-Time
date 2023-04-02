ad37ebc835143ccfe8dd23fc7d896f0a12db0adc
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-02 18:50:33 +0100

    added styling to minute hand

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function setDate() {
 
     const secondHand = document.querySelector('.second-hand');
+    const minsHand = document.querySelector('.min-hand');
+    
     //to get current number of seconds
     const now = new Date();
     const seconds = now.getSeconds();
@@ -9,7 +11,10 @@ function setDate() {
 
     secondHand.style.transform =`rotate(${secondsDegrees}deg)`
 
-   
+    const mins = now.getMinutes();
+    console.log(mins);
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
